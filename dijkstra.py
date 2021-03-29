62f14749b1706146cfe70c0d5b8a899627ab5f49
Jared Rigby <jared.rigby@dchigh-suzhou.cn>
2021-03-29 22:39:33 +0800

    Create dijkstra.py

---
+++ dijkstra.py
@@ -0,0 +1,6 @@
+import math # So you can use math.inf for infinite weight
+
+
+def shortest_path(graph, start, end):
+  # Complete this function using Dijkstra's algorithm to pass the unit tests
+  pass
